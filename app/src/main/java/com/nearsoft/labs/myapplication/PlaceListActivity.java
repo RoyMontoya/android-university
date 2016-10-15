6180c3ab4edfcfbe89573ded372dbf76f9326cc6
chrismacias <christian.m.macias@gmail.com>
2016-10-14 22:17:03 -0700

    First commit

---
+++ app/src/main/java/com/nearsoft/labs/myapplication/PlaceListActivity.java
@@ -0,0 +1,13 @@
+package com.nearsoft.labs.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaceListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_list);
+    }
+}
